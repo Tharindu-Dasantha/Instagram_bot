1e7f1a5ca508e9ada34b240f9e712ced62d56f0f
Tharindu-Dasantha <t.dasanthaedirisinghe@gmail.com>
2023-02-14 17:53:12 +0530

    commicommitt

--- automationpyauto/github_desk/auto_commit/main.py
+++ automationpyauto/github_desk/auto_commit/main.py
@@ -1,20 +1,20 @@
 import pyautogui as pg
 import time 
 
-#time.sleep(3)
-#print(pg.position())
+time.sleep(3)
+print(pg.position())
 
 # Cllickin on the github desktop icon
 pg.leftClick(1051, 1058, 1, 2)
 
 # Clicking on the commit massege
 time.sleep(1)
-pg.leftClick(128, 792, 1, 2)
+pg.leftClick(105, 745, 1, 2)
 pg.write("commit")
 time.sleep(1)
 
 # Clicking on the commi button
-pg.leftClick(189, 990, 1, 2)
+pg.leftClick(198, 931, 1, 2)
 time.sleep(1)
 
 # Clicking on push origin
