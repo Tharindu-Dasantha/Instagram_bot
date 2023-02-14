bfe9361a7a79a23d2ccb5f241f921245e337d040
Tharindu-Dasantha <t.dasanthaedirisinghe@gmail.com>
2023-02-14 17:27:25 +0530

    gg

--- automationpyauto/insta/main.py
+++ automationpyauto/insta/main.py
@@ -4,15 +4,34 @@ import time
 time.sleep(2) # remove this late this is only for marking the times 
 
 # printing out the cordinates needed
-# print(pg.position())
+#print(pg.position())
+
+# taking inputs to log into the insta account
+username = input("Username: ")
+password = input("Password: ")
 
 # clicking on the chrome button
 pg.leftClick(1113, 1046, 1, 1)
+
 # cicking on the chrome account
 pg.leftClick(1298, 469, 1, 1)
+
 # clicking on the search bar
-pg.leftClick(351, 66, 1, 1)
+pg.leftClick(351, 66, 1, 2)
+
 # typing the address
 pg.write("https://www.instagram.com/")
-# pressing enter
 pg.press("enter")
+
+# clicking on the username bar
+pg.leftClick(1153, 338, 1, 1)
+pg.write(username)
+pg.press("enter")
+
+# clicking on the password bar
+pg.leftClick(1122, 399, 1, 1)
+pg.write(password)
+pg.press("enter")
+
+# clicking on the log in button
+pg.leftClick(1148, 449, 1, 1)
