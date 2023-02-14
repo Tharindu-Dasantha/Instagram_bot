32a98b6baeb76baefb973917ab6b28f4cf1e310f
Tharindu-Dasantha <t.dasanthaedirisinghe@gmail.com>
2023-02-14 17:40:16 +0530

    gg

---
+++ automationpyauto/insta/logout_insta.py
@@ -0,0 +1,10 @@
+import pyautogui as pg
+import time
+
+
+# Click on the more button
+pg.leftClick(78, 951, 1, 3)
+
+# Click on the log out button
+time.sleep(1)
+pg.leftClick(105, 898, 1, 1)
--- automationpyauto/insta/main.py
+++ automationpyauto/insta/main.py
@@ -11,10 +11,11 @@ username = input("Username: ")
 password = input("Password: ")
 
 # clicking on the chrome button
-pg.leftClick(1113, 1046, 1, 1)
+pg.leftClick(1113, 1046, 1, 2)
 
 # cicking on the chrome account
 pg.leftClick(1298, 469, 1, 1)
+time.sleep(1)
 
 # clicking on the search bar
 pg.leftClick(351, 66, 1, 2)
@@ -22,16 +23,16 @@ pg.leftClick(351, 66, 1, 2)
 # typing the address
 pg.write("https://www.instagram.com/")
 pg.press("enter")
+time.sleep(1)
 
 # clicking on the username bar
-pg.leftClick(1153, 338, 1, 1)
+pg.leftClick(1153, 338, 1, 2)
 pg.write(username)
-pg.press("enter")
 
 # clicking on the password bar
 pg.leftClick(1122, 399, 1, 1)
 pg.write(password)
-pg.press("enter")
+time.sleep(1)
 
 # clicking on the log in button
 pg.leftClick(1148, 449, 1, 1)
