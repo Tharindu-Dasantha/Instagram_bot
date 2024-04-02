69fc0a9057a1c50385e48a8665362d915ffbc085
Dasantha Edirisinghe <t.dasanthaedirisinghe@gmail.com>
2024-04-02 19:37:44 +0530

    Update the code

--- automationpyauto/github_desk/auto_commit/main.py
+++ automationpyauto/github_desk/auto_commit/main.py
@@ -5,17 +5,17 @@ time.sleep(3)
 print(pg.position())
 
 # Cllickin on the github desktop icon
-pg.leftClick(1051, 1058, 1, 2)
+# pg.leftClick(1051, 1058, 1, 2)
 
 # Clicking on the commit massege
-time.sleep(1)
-pg.leftClick(193, 798, 1, 2)
-pg.write("commit")
-time.sleep(1)
+# time.sleep(1)
+# pg.leftClick(193, 798, 1, 2)
+# pg.write("commit")
+# time.sleep(1)
 
 # Clicking on the commi button
-pg.leftClick(174, 990, 1, 2)
-time.sleep(1)
+# pg.leftClick(174, 990, 1, 2)
+# time.sleep(1)
 
 # Clicking on push origin
-pg.leftClick(1416, 359, 1, 2)
+# pg.leftClick(1416, 359, 1, 2)
